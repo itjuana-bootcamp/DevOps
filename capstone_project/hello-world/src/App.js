a286e1372ab90c2247c64f27ca3badb3d7932cf8
Edgar Olazaba <olazabaedgar@gmail.com>
2023-04-08 09:31:25 -0600

    commit after docker image

--- capstone_project/hello-world/src/App.js
+++ capstone_project/hello-world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello DevOps!
         </p>
       </header>
     </div>
