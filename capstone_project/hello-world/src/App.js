af145f8012ef43c0b043c5646b53a51ae5c0dde0
EOlazaba88 <39096616+EOlazaba88@users.noreply.github.com>
2023-04-09 13:44:10 -0600

    Update App.js

--- capstone_project/hello-world/src/App.js
+++ capstone_project/hello-world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello DevOps!
+          Hello World!
         </p>
       </header>
     </div>
