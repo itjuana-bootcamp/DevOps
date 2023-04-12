9e85c5a54362432161db39ff17dfd5b78f77d3a4
Armando Perez <aperezt@gmail.com>
2023-04-11 21:11:45 -0600

    Update App.js

--- capstone_project/hello-world/src/App.js
+++ capstone_project/hello-world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello DevOps!
+          Hello DevOps!!
         </p>
       </header>
     </div>
