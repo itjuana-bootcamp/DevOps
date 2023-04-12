c94787d294d27714174652c26d82352ca65544c6
Armando Perez <aperezt@gmail.com>
2023-04-11 20:58:42 -0600

    Hello DevOps

--- capstone_project/hello-world/src/App.js
+++ capstone_project/hello-world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Pompi!
+          Hello DevOps!
         </p>
       </header>
     </div>
