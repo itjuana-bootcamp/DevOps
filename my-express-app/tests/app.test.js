8cf8eb27f67f1a76dfd44d7ae3336be8f8c6444b
Alex Garcia <ag0820@dexcom.com>
2024-04-23 18:14:45 -0600

    Cleaning cache after each test

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -8,3 +8,8 @@ describe('GET /', () => {
     expect(response.text).toBe('Hello, World!');
   });
 });
+
+// Clean up the Jest cache after each test
+afterEach(() => {
+    jest.resetModules();
+  });
