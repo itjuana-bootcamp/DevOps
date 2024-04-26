6ab6d75a6b345d602fc6e8a5663914625e3c56ed
Alex Garcia <ag0820@dexcom.com>
2024-04-26 13:29:02 -0600

    Test fail

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   test('It should respond with "Hello, World!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toBe('Hello');
   });
 });
