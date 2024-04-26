47f246283a52a46c5c2ceac50f0dbd53c7d71ef9
Alex Garcia <ag0820@dexcom.com>
2024-04-26 13:46:42 -0600

    Changing the code to ITJers

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  test('It should respond with "Hello, World!"', async () => {
+  test('It should respond with "Hello, ITJers!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello');
+    expect(response.text).toBe('Hello, ITJers!');
   });
 });
