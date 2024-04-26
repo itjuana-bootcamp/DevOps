9148a90434dc9ddee119658c51af5e41b0dfd004
Alex Garcia <ag0820@dexcom.com>
2024-04-26 16:09:32 -0600

    ITJ-001: Fixing tests

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  test('It should respond with "Hello, ITJers!"', async () => {
+  test('It should respond with "Hello, ITJers and happy Friday!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, ITJers!');
+    expect(response.text).toBe('Hello, ITJers and happy Friday!');
   });
 });
