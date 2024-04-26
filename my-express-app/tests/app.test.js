9582004f138fa15a3a264711f58eeee37992786e
Alex Garcia <ag0820@dexcom.com>
2024-04-26 13:28:15 -0600

    Revert "Change test case"
    This reverts commit 238a3df110dd3a26ef5b1a1ed3c53a11a26b83c7.

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  test('It should respond with "Hello"', async () => {
+  test('It should respond with "Hello, World!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
