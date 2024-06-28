a4ea719dafe4657728697955d16bf1df35ea4210
Alex Garcia <ag0820@dexcom.com>
2024-06-28 14:03:57 -0600

    ITJ-001: Adding my node JS app

---
+++ my-express-app/tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+  test('It should respond with "Hello, ITJers!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, ITJers!');
+  });
+});
