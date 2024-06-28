4ffe01f46c932cba1fa821af75ad656cefebe3c9
Alex Garcia <ag0820@dexcom.com>
2024-06-28 11:57:47 -0600

    ITJ-003: Changing message again

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   test('It should respond with "Hello, ITJers!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, ITJers and happy Friday!');
+    expect(response.text).toBe('Hello, ITJers!');
   });
 });
