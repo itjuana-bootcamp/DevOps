72ca68aa72ec8a8f11bea52b7c08f53b18673f18
Alex Garcia <ag0820@dexcom.com>
2025-02-28 17:26:43 -0600

    SWB-001: Changing message

--- my-express-app/tests/app.test.js
+++ my-express-app/tests/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  test('It should respond with: Hello ITJers and super Happy Friday!', async () => {
+  test('It should respond with: Hello ITJers and Happy Friday!', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello ITJers and super Happy Friday!');
+    expect(response.text).toBe('Hello ITJers and Happy Friday!');
   });
 });
