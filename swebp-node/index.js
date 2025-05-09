b8e3a811ee686de5cbedfa05abc695fdf68da911
IsaacRodriguezDexcom <isaac.rodriguez@dexcom.com>
2025-05-09 02:44:17 -0700

    SWE: Init express project

---
+++ swebp-node/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello ITJ');
+});
+
+// Start the server
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
