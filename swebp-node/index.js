dbe30312c2f7b18a0217d44a5edd74d62c6b5da9
IsaacRodriguezDexcom <isaac.rodriguez@dexcom.com>
2025-05-09 13:53:21 -0700

    SWEBP-001: Init app and initial tests

---
+++ swebp-node/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello ITJ');
+});
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Server is listening at http://localhost:${port}`);
+    });
+}
+
+module.exports = app;
---
+++ swebp-node/tests/app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const app = require("../index")
+
+describe('GET /', () => {
+    it('should respond with Hello ITJ', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello ITJ');
+    });
+});
+
+describe('GET /nonexistent', () => {
+    it('should respond with 404', async () => {
+        const response = await request(app).get('/nonexistent');
+        expect(response.statusCode).toBe(404);
+    });
+});
